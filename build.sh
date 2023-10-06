0e00966368cc88c9858f8e7f1fdc63fd1036ab78
Petr Muzikant <30979983+Muzosh@users.noreply.github.com>
2023-10-06 12:55:45 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ g++ -std=c++20 -O2 -fPIC -I$LIBOQS_ROOT_DIR/build/include $(python-config --cfla
 # Manual working version: g++ -std=c++20 -O2 -fPIC -I$LIBOQS_ROOT_DIR/build/include -I/Users/petr/.pyenv/versions/3.11.5/include/python3.11 -c ./build/oqspython_wrap.cxx -o ./build/oqspython_wrap.o
 
 # Link the C++ wrapper with liboqs and Python+OpenSSL+Intl
-g++ -std=c++20 -shared ./build/oqspython_wrap.o -L$LIBOQS_ROOT_DIR/build/lib -loqs -L$(python-config --prefix)/lib -l$(ls $(python-config --prefix)/lib | grep -o 'python[0-9]\+\.[0-9]\+' | tail -1) -o ./build/_oqspython.so
+g++ -std=c++20 -shared ./build/oqspython_wrap.o -L$LIBOQS_ROOT_DIR/build/lib -loqs -L$(python-config --prefix)/lib -l$(ls $(python-config --prefix)/lib | grep -o 'python[0-9]\+\.[0-9]\+' | tail -1) -lssl -lcrypto -o ./build/_oqspython.so
 # Manual working version: g++ -std=c++20 -shared ./build/oqspython_wrap.o -L$LIBOQS_ROOT_DIR/build/lib -loqs -L/Users/petr/.pyenv/versions/3.11.5/lib -lpython3.11 -L/opt/homebrew/lib -lintl -L/opt/homebrew/opt/openssl@1.1/lib -lssl -lcrypto -o ./build/_oqspython.so
 
 # Copy the Python wrapper and the compiled C++ wrapper to the desired location
